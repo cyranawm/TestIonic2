86d120084bf0d84a47a5b659a2a6d685901cfecd
LucasDup <lucas21396@gmail.com>
2017-05-23 04:13:21 +0200

    commit fin de soirée
    aide ok
    Début de carte + menu
    vue login + sign in ok
    Vue mdp oublié ok

--- App/src/pages/accueil/accueil.ts
+++ App/src/pages/accueil/accueil.ts
@@ -1,24 +1,49 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the AccueilPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import * as Leaflet from 'leaflet';
+
+
+
 @IonicPage()
 @Component({
   selector: 'page-accueil',
   templateUrl: 'accueil.html',
 })
-export class AccueilPage {
+export class AccueilPage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+constructor(public navCtrl: NavController) {
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AccueilPage');
+  ngOnInit(): void {
+    this.drawMap();
   }
+  drawMap(): void {
+    let map = Leaflet.map('map');
+    Leaflet.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGF0cmlja3IiLCJhIjoiY2l2aW9lcXlvMDFqdTJvbGI2eXUwc2VjYSJ9.trTzsdDXD2lMJpTfCVsVuA', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(map);
 
+    //web location
+    map.locate({ setView: true});
+
+    //when we have a location draw a marker and accuracy circle
+    function onLocationFound(e) {
+      var radius = e.accuracy / 2;
+
+      Leaflet.marker(e.latlng).addTo(map)
+          .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+      Leaflet.circle(e.latlng, radius).addTo(map);
+    }
+    map.on('locationfound', onLocationFound);
+    //alert on location error
+    function onLocationError(e) {
+      alert(e.message);
+    }
+
+    map.on('locationerror', onLocationError);
+  }
 }

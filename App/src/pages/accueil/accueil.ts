5c5a0b61022d876ce32f79928bb0fe5c2231c6eb
cyranawm <cyran.aouameur@gmail.com>
2017-06-08 23:19:06 +0200

    ajout d'id pour ionic view

--- App/src/pages/accueil/accueil.ts
+++ App/src/pages/accueil/accueil.ts
@@ -43,7 +43,6 @@ export class AccueilPage implements OnInit {
   drawMap(): void {
    var map = L.map('map',{zoomControl:false })
       .setView([51.505, -0.09], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom:20,maxNativeZoom:19}).addTo(map);  
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom:20,maxNativeZoom:19}).addTo(map);  
   }
 }

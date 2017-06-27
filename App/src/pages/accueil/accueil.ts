5ee77eb49fcc61be3675b473a098b787d6e1c956
LucasDup <lucas21396@gmail.com>
2017-06-27 23:07:03 +0200

    Ajout Clusters
    Il manque le style (notamment le background). Mais ca sent bon quand meme. Jette un oeil à mes commentaires et la fonction qui est dedans.

--- App/src/pages/accueil/accueil.ts
+++ App/src/pages/accueil/accueil.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import 'leaflet';
 import 'leaflet-easybutton';
+import 'leaflet.markercluster'
 
 @IonicPage()
 @Component({
@@ -12,53 +13,98 @@ import 'leaflet-easybutton';
   
 })
 export class AccueilPage implements OnInit {
-  Minute_is_pressed: boolean = false;
-  Elec_is_pressed:boolean = false;
-  Pass_is_pressed:boolean = false;
-  Locate_is_pressed:boolean = false;
-  
-
 
   constructor(public navCtrl: NavController) {
 
   }
   
   ngOnInit(): void {
-    var Location_marker:L.Marker;
-    var Location_circle:L.Circle;
-
+    /* ------------------------- Initialisation de la carte ------------------------- */
     var map = L.map('map',{zoomControl:false })
     L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-    var randomMarkers = L.layerGroup([
-      L.marker([37.8, -110]), L.marker([38.8, -86]), L.marker([47.8, -106]),
-      L.marker([31.8, -120]), L.marker([39.8, -96]), L.marker([33.8, -100]) ]);
+    function onLocationFound(e): void {
+      var radius = e.accuracy / 2;
+      var Location_marker = new L.Marker(e.latlng);
+      var Location_circle = new L.Circle(e.latlng,radius);
+      map.addLayer(Location_marker);
+      Location_marker.bindPopup("Vous êtes ici");
+      map.addLayer(Location_circle);
+    }
+      
+    function onLocationError(e): void  { 
+      alert(e.message);
+    }
 
-    
+    map.on('locationfound',onLocationFound);
+    map.locate({setView:true, maxZoom: 16}) 
+
+
+    /* ------------------- Implémentation boutton géolocalisation ------------------- */
+
+    var locate_button = new L.Control.EasyButton({
+      id: 'id-for-the-button',  
+      position: 'bottomright',     
+      type: 'replace',          
+      leafletClasses: true,     
+      states:[{                 
+        stateName: 'get-center',
+        onClick: function(btn, map){
+          map.locate({setView:true, maxZoom: 16})
+          },
+        title: 'show me the middle',
+        icon: 'fa-crosshairs'
+      }]
+    }); 
+
+    locate_button.addTo(map);
+
+
+    /* --------------------- Implémentation bouttons Statioguide --------------------- */
+    /* L'idée est d'initialiser les clusters ici .... */
+    /* Aller voir la fonction /Users/Lucas/Documents/Mines/Divers/JE/TestIonic2/Web/app/assets/javascripts/Clusters/Clusters.js */
+    var c_marker_flash = L.markerClusterGroup({
+      polygonOptions: {
+          fillColor: '#3887be',
+          color: '#3887be',
+          weight: 2,
+          opacity: 1,
+          fillOpacity: 0.5
+        },disableClusteringAtZoom: 18,
+     iconCreateFunction: function (cluster) {
+      var childMarkers = cluster.getAllChildMarkers();
+      return L.divIcon({ html: "<p> Test </p>",className: "marker-cluster marker-cluster-green",  iconSize: L.point(40, 40)});
+     },
+    });
+
+    var randomMarkers = L.layerGroup([
+    L.marker([37.8, -110]), L.marker([38.8, -86]), L.marker([47.8, -106]),
+    L.marker([31.8, -120]), L.marker([39.8, -96]), L.marker([33.8, -100]) ]);
 
     var flashButton = new L.Control.EasyButton({
-      id: 'button1',  // an id for the generated button
-      type: 'replace',          // set to animate when you're comfy with css
-      leafletClasses: true,     // use leaflet classes to style the button?
+      id: 'button1',  
+      type: 'replace',
+      leafletClasses: true,     
       states: [{
-            stateName: 'noflash',        // name the state
-            icon:      'fa-bolt',               // and define its properties
-            title:     'not-flash',      // like its title
-            onClick: function(btn, map) {       // and its callback
-                map.setView([46.25,-121.8],10);
-                map.addLayer(randomMarkers);
-                btn.state('flash');    // change state on click!
-            }
-        }, {
+            stateName: 'noflash',        
+            icon:      'fa-bolt',        
+            title:     'not-flash',       
+            onClick: function(btn, map) { 
+              /* Et de les remplir ici, ca permet de prendre que les places à moins de 20km */
+              c_marker_flash.addLayer(randomMarkers);
+              map.addLayer(c_marker_flash);
+              btn.state('flash');    
+              }
+            },{
             stateName: 'flash',
             icon:      'fa-bolt',
             title:     'flash',
             onClick: function(btn, map) {
-                map.setView([42.3748204,-71.1161913],16);
-                map.removeLayer(randomMarkers);
-                btn.state('noflash');
-            }
-    }]
+              map.removeLayer(c_marker_flash);
+              c_marker_flash.removeLayers;
+              btn.state('noflash');
+              }
+            }]
     });
 
     var chronoButton = new L.Control.EasyButton({
@@ -70,11 +116,11 @@ export class AccueilPage implements OnInit {
             icon:      'fa-clock-o',               // and define its properties
             title:     'not-chrono',      // like its title
             onClick: function(btn, map) {       // and its callback
-                map.setView([46.25,-121.8],10);
-                map.addLayer(randomMarkers);
-                btn.state('chrono');    // change state on click!
-            }
-        }, {
+              map.setView([46.25,-121.8],10);
+              map.addLayer(randomMarkers);
+              btn.state('chrono');    // change state on click!
+              }
+            },{
             stateName: 'chrono',
             icon:      'fa-clock-o',
             title:     'chrono',
@@ -83,9 +129,8 @@ export class AccueilPage implements OnInit {
                 map.removeLayer(randomMarkers);
                 btn.state('nochrono');
             }
-    }]
+          }]
     });
-
 
     var handiButton = new L.Control.EasyButton({
       id: 'button3',  // an id for the generated button
@@ -96,20 +141,20 @@ export class AccueilPage implements OnInit {
             icon:      'fa-wheelchair',               // and define its properties
             title:     'not-handi',      // like its title
             onClick: function(btn, map) {       // and its callback
-                map.setView([46.25,-121.8],10);
-                map.addLayer(randomMarkers);
-                btn.state('handi');    // change state on click!
+              map.setView([46.25,-121.8],10);
+              map.addLayer(randomMarkers);
+              btn.state('handi');    // change state on click!
             }
-        }, {
+            },{
             stateName: 'handi',
             icon:      'fa-wheelchair',
             title:     'handi',
             onClick: function(btn, map) {
-                map.setView([42.3748204,-71.1161913],16);
-                map.removeLayer(randomMarkers);
-                btn.state('nohandi');
+              map.setView([42.3748204,-71.1161913],16);
+              map.removeLayer(randomMarkers);
+              btn.state('nohandi');
             }
-    }]
+          }]
     });
 
     var buttonBar = L.easyBar([
@@ -120,60 +165,17 @@ export class AccueilPage implements OnInit {
 
     buttonBar.addTo( map );
 
-
-
-
-
-
-
-
-    function onLocationFound(e): void {
-      var radius = e.accuracy / 2;
-      Location_marker = new L.Marker(e.latlng);
-      Location_circle = new L.Circle(e.latlng,radius);
-      map.addLayer(Location_marker);
-      map.addLayer(Location_circle);
-    }
-      
-    function onLocationError(e): void  { 
-      alert(e.message);
-    }
-
-
     /*var marker;
-    
-
     function test(e):void  {
       if (marker) { // check
           map.removeLayer(marker); // remove
       }
       marker = new L.Marker(e.latlng ); // set
       map.addLayer(marker);  
-    };
-  
-    map.on('click',test); */
-
-
-    map.on('locationfound',onLocationFound);
-    map.locate({setView:true, maxZoom: 16}) 
-  
+    }
+    map.on('click',test); */  
+  }
 }
-  
-  /*Instructions pour gestion FabButtons*/
-  Minute_Pressed(){
-    this.Minute_is_pressed=!(this.Minute_is_pressed);
-    /*Aller chercher dans BDD les emplacements*/
-  }
-  Elec_Pressed(){
-    this.Elec_is_pressed=!(this.Elec_is_pressed);
-    /*Aller chercher dans BDD les emplacements*/
-  }
-  Pass_Pressed(){
-    this.Pass_is_pressed=!(this.Pass_is_pressed);
-    /*Aller chercher dans BDD les emplacements*/
-  }  
-
- }
 
   
 
